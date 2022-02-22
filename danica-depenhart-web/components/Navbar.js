b892ae413b015bdb951c657a1e6aa4d074d574ba
Christian Anderson <somecallmehans@gmail.com>
2022-02-22 15:50:20 -0500

    Updated Navbar

--- danica-depenhart-web/components/Navbar.js
+++ danica-depenhart-web/components/Navbar.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <>
-      <nav className="flex items-center flex-wrap bg-transparent-300 p-3">
+      <nav className="flex items-center justify-between flex-wrap bg--500 p-6">
         <Link href="/">
           <a className="inline-flex items-center p-2 mr-4 ">
             <span className="text-xl text-black font-bold uppercase tracking-wide">
@@ -13,14 +13,14 @@ export default function Navbar() {
           </a>
         </Link>
         <div className="hidden w-full lg:inline-flex lg:flex-grow lg:w-auto">
-          <div className="lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start  flex flex-col lg:h-auto">
+          <div className="flex flex-row justify-center items-center">
             <Link href="/publications">
-              <a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-black font-bold items-center justify-center hover:bg-green-600 hover:text-white ">
+              <a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-black font-bold items-center justify-center hover:bg-gray-400 hover:text-white">
                 Publications
               </a>
             </Link>
             <Link href="/about">
-              <a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-black font-bold items-center justify-center hover:bg-green-600 hover:text-white">
+              <a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-black font-bold items-center justify-center hover:bg-gray-400 hover:text-white">
                 About
               </a>
             </Link>
