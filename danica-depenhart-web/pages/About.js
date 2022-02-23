689560de455e110c1a708a369d92663db49231a4
Christian Anderson <somecallmehans@gmail.com>
2022-02-23 16:58:03 -0500

    Added about section

--- danica-depenhart-web/pages/About.js
+++ danica-depenhart-web/pages/About.js
@@ -4,7 +4,55 @@ export default function About() {
   return (
     <div>
       <Layout>
-        <h1>This page is about Danica</h1>
+        <section className="bg-transparent px-80 py-10">
+          <div className=" overflow-hidden mx-auto px-1 h-auto max-h-screen about-background">
+            <div className="p-4">
+              <h1 className="text-2xl font-bold">about</h1>
+              <br></br>
+              <div className="px-8">
+                <p className="text-base">
+                  But I must explain to you how all this mistaken idea of
+                  denouncing pleasure and praising pain was born and I will give
+                  you a complete account of the system, and expound the actual
+                  teachings of the great explorer of the truth, the
+                  master-builder of human happiness. No one rejects, dislikes,
+                  or avoids pleasure itself, because it is pleasure, but because
+                  those who do not know how to pursue pleasure rationally
+                  encounter consequences that are extremely painful. Nor again
+                  is there anyone who loves or pursues or desires to obtain pain
+                  of itself, because it is pain, but because occasionally
+                  circumstances occur in which toil and pain can procure him
+                  some great pleasure. To take a trivial example, which of us
+                  ever undertakes laborious physical exercise, except to obtain
+                  some advantage from it? But who has any right to find fault
+                  with a man who chooses to enjoy a pleasure that has no
+                  annoying consequences, or one who avoids a pain that produces
+                  no resultant pleasure?
+                </p>
+                <br></br>
+                <p>
+                  But I must explain to you how all this mistaken idea of
+                  denouncing pleasure and praising pain was born and I will give
+                  you a complete account of the system, and expound the actual
+                  teachings of the great explorer of the truth, the
+                  master-builder of human happiness. No one rejects, dislikes,
+                  or avoids pleasure itself, because it is pleasure, but because
+                  those who do not know how to pursue pleasure rationally
+                  encounter consequences that are extremely painful. Nor again
+                  is there anyone who loves or pursues or desires to obtain pain
+                  of itself, because it is pain, but because occasionally
+                  circumstances occur in which toil and pain can procure him
+                  some great pleasure. To take a trivial example, which of us
+                  ever undertakes laborious physical exercise, except to obtain
+                  some advantage from it? But who has any right to find fault
+                  with a man who chooses to enjoy a pleasure that has no
+                  annoying consequences, or one who avoids a pain that produces
+                  no resultant pleasure?
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
       </Layout>
     </div>
   );
