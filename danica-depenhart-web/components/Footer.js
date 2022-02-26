fea8899e42f7c6859298a34759d5c02327967974
Christian Anderson <somecallmehans@gmail.com>
2022-02-26 13:04:43 -0500

    Pin footer to bottom

--- danica-depenhart-web/components/Footer.js
+++ danica-depenhart-web/components/Footer.js
@@ -2,24 +2,22 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div className="overflow-hidden bg-transparent-200 absolute inset-x-1 bottom-0">
-      <div className="max-w-6xl">
-        <div className="flex flex-col align-middle md:space-y-4 md:flex-row justify-between md:items-center mt-8">
+    <div className="overflow-hidden bg-transparent-200 fixed inset-x-1 bottom-0 border-2 border-rose-500">
+      <div className="flex flex-col align-middle md:space-y-4 md:flex-row justify-between md:items-center mt-8">
+        <div className="text-slate-500 text-xxs">
           <div className="text-slate-500 text-xxs">
-            <div className="text-slate-500 text-xxs">
-              <p>&copy; danica depenhart. All Rights Reserved</p>
-            </div>
-            <p>
-              <div className="inline-block">
-                <a
-                  className="hover:bg-black-500 rounded-md px-2 py-1 hover:text-white-50"
-                  href="https://www.linkedin.com/in/christian-anderson-pgh/"
-                >
-                  developed by christian anderson
-                </a>
-              </div>
-            </p>
+            <p>&copy; danica depenhart. All Rights Reserved</p>
           </div>
+          <p>
+            <div className="inline-block">
+              <a
+                className="hover:bg-black-500 rounded-md px-2 py-1 hover:text-white-50"
+                href="https://www.linkedin.com/in/christian-anderson-pgh/"
+              >
+                developed by christian anderson
+              </a>
+            </div>
+          </p>
         </div>
       </div>
     </div>
