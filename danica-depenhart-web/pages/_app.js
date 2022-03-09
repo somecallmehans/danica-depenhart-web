99d6b7c5d423a8497c5097fc8e9ef356c4be5501
Christian Anderson <somecallmehans@gmail.com>
2022-03-09 11:19:51 -0500

    Added loading transitions between pages

--- danica-depenhart-web/pages/_app.js
+++ danica-depenhart-web/pages/_app.js
@@ -1,8 +1,32 @@
 /*   ./styles/_app.js              */
 import "../styles/main.css";
+import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+  const [pageLoad, setPageLoad] = useState(false);
+  useEffect(() => {
+    const handleState = () => {
+      setPageLoad(true);
+    };
+    const handleComplete = () => {
+      setPageLoad(false);
+    };
+
+    router.events.on("routeChangeStart", handleState);
+    router.events.on("routeChangeComplete", handleComplete);
+    router.events.on("routeChangeError", handleComplete);
+  }, [router]);
+
+  return pageLoad ? (
+    <div className="flex flex-col items-center justify-center overflow-x-hidden h-screen font-serif">
+      <div className="text-6xl text-[#30110d]"> Loading </div>;
+    </div>
+  ) : (
+    <Component {...pageProps} />
+  );
+  /*  return <Component {...pageProps} />; */
 }
 
 export default MyApp;
