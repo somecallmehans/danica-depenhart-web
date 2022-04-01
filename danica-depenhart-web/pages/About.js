84fa50fde0eb2a6355a465179e34d5de28088024
Christian Anderson <somecallmehans@gmail.com>
2022-04-01 11:27:13 -0400

    Updated bio

--- danica-depenhart-web/pages/About.js
+++ danica-depenhart-web/pages/About.js
@@ -1,5 +1,6 @@
 import Layout from "../components/Layout";
 import Image from "next/image";
+import Link from "next/link";
 import bdb from "../public/bdb.jpg";
 
 export default function About() {
@@ -18,18 +19,14 @@ export default function About() {
           </div>
           <div className="mt-2 md:w-1/2">
             <p className="px-4 text-[#30110d] md:w-2/3">
-              But I must explain to you how all this mistaken idea of denouncing
-              pleasure and praising pain was born and I will give you a complete
-              account of the system, and expound the actual teachings of the
-              great explorer of the truth, the master-builder of human
-              happiness. No one rejects, dislikes, or avoids pleasure itself,
-              because it is pleasure, but because those who do not know how to
-              pursue pleasure rationally encounter consequences that are
-              extremely painful. Nor again is there anyone who loves or pursues
-              or desires to obtain pain of itself, because it is pain, but
-              because occasionally circumstances occur in which toil and pain
-              can procure him some great pleasure. To take a trivial example,
-              which of us
+              Danica Depenhart is a Pittsburgh based writer, maker, & educator.
+              She is a recipient of The Alex Rowan Award for poetry writing, &
+              her work has been featured in TriQuarterly, Pretty Owl Poetry, and
+              Burningword Literary Journal. You can find her on instagram
+              <Link href="https://www.instagram.com/motherweather">
+                <a> @motherweather</a>
+              </Link>
+              .
             </p>
           </div>
         </section>
