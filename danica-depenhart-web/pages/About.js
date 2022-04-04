061d72bc5d0a285c932abd773552c1a43519aee0
Christian Anderson <somecallmehans@gmail.com>
2022-04-04 16:22:54 -0400

    updated about section

--- danica-depenhart-web/pages/About.js
+++ danica-depenhart-web/pages/About.js
@@ -1,24 +1,24 @@
 import Layout from "../components/Layout";
 import Image from "next/image";
 import Link from "next/link";
-import bdb from "../public/bdb.jpg";
+import bdb from "../public/000224440022.jpg";
 
 export default function About() {
   return (
     <div>
       <Layout>
-        <section className="relative flex flex-col w-full h-full mx-auto mt-8 md:flex-row md:w-3/4">
-          <div className="md:w-1/2 h-[300px] md:h-[600px] p-4 relative">
+        <section className="relative flex flex-col w-full h-full mx-auto mt-2 md:flex-row md:w-3/4">
+          <div className="md:w-1/2 min-h-[300px] min-w-[200px] md:min-h-[600px] p-4 drop-shadow-lg">
             <Image
               src={bdb}
               alt="Danica image"
               layout="fill"
               objectFit="contain"
-              className="md:object-right"
+              className="md:object-right min-h-[300px] min-w-[200px]"
             />
           </div>
-          <div className="mt-2 md:w-1/2">
-            <p className="px-4 text-[#30110d] md:w-2/3">
+          <div className="md:w-1/2 md:my-auto">
+            <p className="px-8 text-[#30110d] text-xl w-full lg:w-2/3 2xl:w-1/2">
               Danica Depenhart is a Pittsburgh based writer, maker, & educator.
               She is a recipient of The Alex Rowan Award for poetry writing, &
               her work has been featured in TriQuarterly, Pretty Owl Poetry, and
